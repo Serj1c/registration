0d56269e01ff047ec858ce2868da76c4f5e475e5
Sergei Isaev <sergej.isaeff@gmail.com>
2022-01-13 15:00:39 +0300

    initial commit

---
+++ registration.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+void mainMenu();
+
+class Management
+{
+    public:
+        Management()
+        {
+            mainMenu();
+        }
+};
+
+void mainMenu()
+{
+    int lchoice;
+    int schoice;
+    int back;
+}
+
+int main()
+{
+    Management Mobj;
+    return 0;
+}
